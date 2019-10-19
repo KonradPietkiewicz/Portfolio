1cae141351d655c19e5a39082ec40d0d78e3a4de
Konrad Pietkiewicz <konradpietkiewicz20@gmail.com>
2019-10-19 23:34:23 +0200

    Add style

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let today = new Date();
 let year = today.getFullYear();
 
-let el = document.getElementById('footer');
+let el = document.getElementById('protection_copy');
 el.innerHTML = '<p>Copyright &copy; ' + year + '</p>'
+protection_copy.style.fontSize = "1.5rem";
+protection_copy.style.lineHeight = "15vh"
+protection_copy.style.paddingLeft = "250px"
