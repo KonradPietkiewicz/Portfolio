c991cf120b33b16c5b9af202396e6949002f99f3
Konrad Pietkiewicz <konradpietkiewicz20@gmail.com>
2019-11-28 21:25:13 +0100

    Change  welcome

--- text.js
+++ text.js
@@ -1,11 +1,11 @@
-const pText = document.querySelector('.person_name');
-const txt = " Konrad Pietkiewicz"
+const pText = document.querySelector(".person_welcome");
+const txt = "Witam na mojej stronie :)";
 
 let indexText = 0;
 const time = 90;
 const addLetter = () => {
-    pText.textContent += txt[indexText];
-    indexText++;
-    if (indexText === txt.length) clearInterval(indexTyping);
-}
+  pText.textContent += txt[indexText];
+  indexText++;
+  if (indexText === txt.length) clearInterval(indexTyping);
+};
 const indexTyping = setInterval(addLetter, time);
